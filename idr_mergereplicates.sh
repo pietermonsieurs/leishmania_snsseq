1112314ba695b04104f811bfffaa368f234af6af
Pieter Monsieurs <pmonsieurs@itg.be>
2021-05-12 09:06:51 +0200

    updated IDR script

---
+++ idr_mergereplicates.sh
@@ -0,0 +1,9 @@
+## installation of IDR: 
+## download from:
+## installation commands:
+# module load Python/3.7.4-intel-2019b
+# export PYTHONPATH=/data/antwerpen/205/vsc20587/software_from_home/python_lib/lib/python3.7/site-packages/:$PYTHONPATH
+# python3 setup.py install --prefix=/data/antwerpen/205/vsc20587/software_from_home/python_lib/
+
+## test run with no input - which will  print help file
+/user/antwerpen/205/vsc20587/data/software/idr-2.0.4/bin/idr
