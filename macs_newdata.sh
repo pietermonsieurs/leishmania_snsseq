326c20a451ad63719dd5526d5538745719466b81
Pieter Monsieurs <pmonsieurs@itg.be>
2021-05-25 16:45:17 +0200

    updated macs_newdata.sh

--- macs_newdata.sh
+++ macs_newdata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-#PBS -l walltime=0:59:00
+#PBS -l walltime=0:20:00
 #PBS -L tasks=6:lprocs=4
 
 
@@ -14,16 +14,26 @@ module load Python/3.7.4-intel-2019b
 export PYTHONPATH=/data/antwerpen/205/vsc20587/software_from_home/python_lib/lib/python3.7/site-packages/:$PYTHONPATH
 BWA_DIR=/user/antwerpen/205/vsc20587/scratch/leishmania_snsseq/results/bwa_newdata/
 OUT_DIR=/user/antwerpen/205/vsc20587/scratch/leishmania_snsseq/results/macs_newdata/
+seed=100
 
 /data/antwerpen/205/vsc20587/software_from_home/python_lib/bin/macs2 callpeak \
      -f BAM \
      -g 38e6 \
      --nomodel --extsize 150 \
      -n ${fg}_vs_${bg} \
-     -t $BWA_DIR/${fg}.mapq30.removedups.proper_paired.subsample.bam \
-     -c $BWA_DIR/${bg}.mapq30.removedups.proper_paired.subsample.bam \
+     -t $BWA_DIR/${fg}.mapq30.removedups.proper_paired.bam \
+     -c $BWA_DIR/${bg}.mapq30.removedups.proper_paired.bam \
      --outdir $OUT_DIR \
-     --scale-to small
+     --scale-to small \
+     --seed ${seed} \
+     --bdg \
+     --SPMR \
+     --broad
+
+## explanation on some of the parameters
+# --scale-to-small: this is an alternative to the subsampling approach
+# --seed: should be set to make the subsampling (scale-to-small) reproducible
+# --SPMR: makes the bedgraph mpilup output reproducible between different samples (as a per million reads approach) 
 
 
 ## module worker
